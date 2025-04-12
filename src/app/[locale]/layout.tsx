d237d6f332ab77939e1cba6669157e82018f3e8e
Christopher Alphonse <christopheralphonse96@gmail.com>
2025-04-12 14:47:38 -0400

    Refactor code review workflows, enhance pull request template, improve internationalization error handling, and update locale messages

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,12 +1,12 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import { notFound } from 'next/navigation';
 
-import { htmlLinkObjectData, htmlLinkObjectType } from '@/data/htmlData';
 import { metadata } from '@/data/siteMetadata';
+import getMessages from '@/i18n/request';
 import { routing } from '@/i18n/routing';
 
 import '../globals.css';
-import { NextIntlClientProvider, hasLocale } from 'next-intl';
+import { NextIntlClientProvider } from 'next-intl';
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -17,28 +17,29 @@ const geistMono = Geist_Mono({
     variable: '--font-geist-mono',
     subsets: ['latin']
 });
+
 export { metadata };
-export default async function RootLayout({
-    children,
-    params
-}: Readonly<{
+
+type Props = Readonly<{
     children: React.ReactNode;
-    params: { locale: string };
-}>) {
-    const { locale } = params;
-    if (!hasLocale(routing.locales, locale)) {
+    params: Promise<{ locale: string }>;
+}>;
+
+export default async function RootLayout({ children, params }: Props) {
+    const { locale } = await params;
+
+    if (!routing.locales.includes(locale as 'en' | 'fr')) {
         notFound();
     }
+
+    const messages = await getMessages({ requestLocale: Promise.resolve(locale) });
 
     return (
         <html lang={locale} className='dark scroll-smooth' suppressHydrationWarning>
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-                <NextIntlClientProvider>{children}</NextIntlClientProvider>
-                {htmlLinkObjectData.map((urlData: htmlLinkObjectType) => {
-                    const { href, rel, sizes, type } = urlData;
-
-                    return <link key={href} rel={rel} href={href} type={type} sizes={sizes} />;
-                })}
+                <NextIntlClientProvider locale={locale} messages={messages}>
+                    {children}
+                </NextIntlClientProvider>
             </body>
         </html>
     );
